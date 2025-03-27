0b972104e8d420d96435b8732acdfc48c7f05504
“zhangyisong” <zys06654@gmail.com>
2025-03-27 01:38:12 +0000

    修改了机甲位置，游戏传送点位置和枪的同步

--- Assets/gun.cs
+++ Assets/gun.cs
@@ -1,32 +1,44 @@
 using UnityEngine;
 using Ubiq.Messaging;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Gun : MonoBehaviour
 {
     private NetworkContext context;
+    private bool owner;
+    public UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grab;
 
     // 定义消息格式
     private struct SyncMessage
     {
         public Vector3 position;
+        public Quaternion rotation;
 
-        public SyncMessage(Vector3 pos)
+
+       public SyncMessage(Transform transform)
         {
-            position = pos;
+            this.position = transform.position;
+            this.rotation = transform.rotation;
         }
     }
 
     private void Start()
     {
+        grab.selectEntered.AddListener(XRGrabInteractable_SelectEntered);
+        grab.selectExited.AddListener(XRGrabInteractable_SelectExited);
         // 注册到 Ubiq 网络
         context = NetworkScene.Register(this);
     }
 
     private void FixedUpdate()
     {
-        // 持续广播位置
-        Debug.Log("1111");
-        context.SendJson(new SyncMessage(transform.position));
+        if(owner)
+        {
+            // 持续广播位置
+            Debug.Log("1111");
+            context.SendJson(new SyncMessage(transform));
+        }
+        
     }
 
     public void ProcessMessage(ReferenceCountedSceneGraphMessage msg)
@@ -35,4 +47,15 @@ public class Gun : MonoBehaviour
         Debug.Log("3333333333333333");
         transform.position = data.position;
     }
+
+      private void XRGrabInteractable_SelectEntered(SelectEnterEventArgs arg0)
+    {
+        owner = true;
+    }
+
+    private void XRGrabInteractable_SelectExited(SelectExitEventArgs eventArgs)
+    {
+        owner = false;
+    }
+
 }
